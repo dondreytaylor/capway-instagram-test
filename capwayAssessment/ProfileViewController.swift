1edfffc5d31da8d2cb3ea6e5731b931bf089b87e
Ronnie 😖😷💖 <ronaldjones@ronalds-macbook-air.local>
2019-04-11 07:00:00 -0400

    Project Setup. (Removed Storyboard and added ProfileViewController and PhotoFeedViewController)

---
+++ capwayAssessment/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  capwayAssessment
+//
+//  Created by Ronald Jones on 4/11/19.
+//  Copyright © 2019 Ron Jones Jr. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
